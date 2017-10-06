096fa03e337c7386d1f0d03447872034bd2c9195
richi <richi@192.168.1.189>
2017-10-06 17:50:08 +0100

    BST created, test class created

--- src/softwareEng/binarySearchTree.java
+++ src/softwareEng/binarySearchTree.java
@@ -1,7 +1,41 @@
 package softwareEng;
 
-/*Commit test*/
-
 public class binarySearchTree {
+	
+	Node root;
 
+	class Node {
+		int key;
+		Node left, right;
+		Node(int key){
+			this.key = key;
+		}
+	}
+		
+	public void addNode(int key){
+		Node newNode = new Node(key);
+		if(root == null){
+			newNode = root;
+		}
+		else{
+		   Node focusNode = root;
+		   Node parent;
+		   while(true){
+			   parent = focusNode;
+			   if(key < focusNode.key){
+			   		focusNode = focusNode.left;
+			   		if(focusNode == null){
+			   			parent.left = newNode;
+			   			return;
+			   		}
+			   	}else{
+				    focusNode = focusNode.right;
+				    if(focusNode == null){
+					    parent.right =  newNode;
+					    return;
+				   }
+			   }
+		   }
+		}
+	 }
 }
---
+++ src/softwareEng/binarySearchTreeTest.java
@@ -0,0 +1,10 @@
+package softwareEng;
+
+import junit.framework.TestCase;
+
+public class binarySearchTreeTest extends TestCase {
+	/* want to test that if two nodes are passed into the program, it will return the LCA*/
+	binarySearchTree tree = new binarySearchTree();
+	
+	
+}
