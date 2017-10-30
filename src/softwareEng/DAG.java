d6be4a843cadd811853088a776d66e2737251f48
Richard P. Noonan <richard@encoregolfballs.ie>
2017-10-30 22:01:10 +0000

    Node Class and isEmpty function

--- src/softwareEng/DAG.java
+++ src/softwareEng/DAG.java
@@ -1,6 +1,32 @@
 package softwareEng;
+import java.util.Arrays;
 
-public class DAG {
-		/*Test Branch*/
-	
-}
+	public class DAG <Value> {
+		private class Node {
+			private Value val;					
+			private Node[] successors;			
+		
+			public Node(Value val) {
+				this.val = val;
+			}
+		}
+		
+		private Node[] nodeList = new DAG.Node[0];	
+		
+		public boolean isEmpty() {
+			if(nodeList.length == 0){
+				return true;
+			}else{
+				return false;
+			}
+		}
+		/*public boolean contains(Value v){
+			
+		}
+		
+		public void put(Value v, Value fromVal, Value toVal) {
+			
+		}
+		*/
+	}
+
