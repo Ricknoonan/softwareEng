0abdff23edc152b982db4f6fa0123ceddc7f7481
Richard P. Noonan <richard@encoregolfballs.ie>
2017-11-28 23:00:22 +0000

    Merge

--- src/softwareEng/LCATests.java
+++ src/softwareEng/LCATests.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 public class LCATests {
 	
 	
-	//Binary Tree Test 
-	//----------------------------------------
 	@Test
 	public void findTest() {
 		binarySearchTree tree = new binarySearchTree();
