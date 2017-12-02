c352d6933dedb6f8ffae69776cbb0934eb915857
Richard P. Noonan <richard@encoregolfballs.ie>
2017-12-02 22:51:14 +0000

    Printing out JSON on inputed repository

--- src/softwareEng/GithubInt.java
+++ src/softwareEng/GithubInt.java
@@ -1,5 +1,32 @@
 package softwareEng;
 
-public class GithubInt {
+import java.net.URL;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 
+public class GithubInt {
+	
+	public static void main(String[] args) throws Exception{
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("What is your GitHub Username?");
+				String username = scanner.next();
+				System.out.println("What repo do you want?");
+				String repo = scanner.next();
+				HttpURLConnection httpcon = (HttpURLConnection) new URL("https://api.github.com/repos/" 
+					+ username + "/" + repo).openConnection();														
+				scanner.close();
+				
+				httpcon.addRequestProperty("User-Agent", "Chrome");
+				BufferedReader in = new BufferedReader(new InputStreamReader(httpcon.getInputStream()));
+				
+				StringBuilder responseSB = new StringBuilder();
+				String line;
+				while (( line = in.readLine()) != null) {
+					responseSB.append("\n" + line);
+					System.out.println(line);
+				}
+				in.close();	
+	}	
 }
