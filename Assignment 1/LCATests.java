940724122fe0b9a9f156958556cfd9202cbcc6b8
Ricknoonan <noonanri@tcd.ie>
2017-12-12 14:28:42 +0000

    Assignment1 reorganized

---
+++ Assignment 1/LCATests.java
@@ -0,0 +1,88 @@
+package softwareEng;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class LCATests {
+	
+	
+	@Test
+	public void findTest() {
+		binarySearchTree tree = new binarySearchTree();
+		tree.addNode(27, "1st");tree.addNode(5, "2nd");tree.addNode(29, "3rd");
+		assertTrue(tree.findNode(27));
+		assertFalse(tree.findNode(18));
+	}
+	
+	@Test
+	public void testLCA (){
+		binarySearchTree tree = new binarySearchTree();
+		tree.addNode(6,"p");tree.addNode(4,"e");tree.addNode(1,"s");  
+		tree.addNode(11,"g");tree.addNode(34,"l");tree.addNode(7,"q"); 
+		tree.addNode(51,"7"); tree.addNode(5, "R");	
+		
+		Node test1 = tree.lca(tree.root, 4, 11);					
+		assertEquals(6, test1.key);
+		assertNotEquals(9,test1.key);
+		
+		Node test2 = tree.lca(tree.root, 7, 34);
+		assertEquals(11, test2.key);
+		assertNotEquals(9,test2.key);
+		
+		Node test3 = tree.lca(tree.root, 1, 5);
+		assertEquals(4, test3.key);
+		assertNotEquals(9,test3.key);
+		
+		Node test4 = tree.lca(tree.root, 11, 4);
+		assertEquals(6, test4.key);
+		assertNotEquals(9,test4.key);
+		
+	}
+
+	@Test
+	public void testOrderedLCA(){
+		binarySearchTree tree = new binarySearchTree();
+		tree.addNode(6,"p");tree.addNode(8,"e");tree.addNode(10,"s");
+		tree.addNode(34,"l");tree.addNode(42,"q");tree.addNode(51,"7");
+		
+		Node test = tree.lca(tree.root, 6,8);
+		assertEquals (6, test.key);
+		
+	}
+	
+	@Test
+	public void testMinusNumber(){
+		binarySearchTree tree = new binarySearchTree();
+		tree.addNode(-7,"p");tree.addNode(-6,"e");tree.addNode(10,"s");
+		tree.addNode(34,"l");tree.addNode(-8,"q");tree.addNode(51,"7");
+		
+		Node test = tree.lca(tree.root, -8, 34);
+		assertEquals(-7, test.key);
+	}
+	
+	@Test
+	public void testMoreThanTwo(){
+		binarySearchTree tree = new binarySearchTree();
+		tree.addNode(34,"p");tree.addNode(8,"e");tree.addNode(10,"s");
+		tree.addNode(3,"l");tree.addNode(4,"q");tree.addNode(51,"7");
+		tree.addNode(45,"7");tree.addNode(20,"7");tree.addNode(60,"7");
+		tree.addNode(1, "");tree.addNode(16, "");tree.addNode(40, "");
+
+		Node test1 = tree.lca2(tree.root, 3, 8, 34);
+		assertEquals(34, test1.key);
+		
+		Node test2 = tree.lca2(tree.root, 3, 8, 10);
+		assertEquals(8, test2.key);
+		
+		Node test3 = tree.lca2(tree.root, 40, 1, 16);
+		assertEquals(34, test3.key);
+		
+		Node test4 = tree.lca2(tree.root, 51, 60, 40);
+		assertEquals(51, test4.key);
+
+	}
+	
+	
+}
+
+
---
+++ Assignment 1/binarySearchTree.java
@@ -0,0 +1,89 @@
+package softwareEng;
+
+public class binarySearchTree {
+
+	Node root = null;
+
+	public void addNode(int key, String name) {
+		Node newNode = new Node(key, name);
+		if (root == null) {
+			root = newNode;
+		} else {
+			Node focusNode = root;
+			Node parent;
+			while (true) {
+				parent = focusNode;
+				if (key < focusNode.key) {
+					focusNode = focusNode.leftChild;
+					if (focusNode == null) {
+						parent.leftChild = newNode;
+						return;
+					}
+				} else { 
+					focusNode = focusNode.rightChild;
+					
+					if (focusNode == null) {
+						parent.rightChild = newNode;
+						return; 
+					}
+				}
+			}
+		}
+	}
+	
+	public boolean findNode(int key) {
+		Node focusNode = root;
+		while (focusNode.key != key) {
+			if (key < focusNode.key) {
+				focusNode = focusNode.leftChild;
+			} else {
+				focusNode = focusNode.rightChild;
+			}
+			if (focusNode == null)
+				return false;
+		}
+		return true;
+	}
+
+	public Node lca(Node node, int n1, int n2) 
+    {
+        if (node == null)
+            return null;
+  
+        if (node.key > n1 && node.key > n2)
+            return lca(node.leftChild, n1, n2);
+  
+        if (node.key < n1 && node.key < n2) 
+            return lca(node.rightChild, n1, n2);
+  
+        return node;
+    }
+	
+	public Node lca2(Node node, int n1, int n2, int n3){
+		if(node == null){
+			return null;
+		}
+		if(node.key > n1 && node.key > n2 && node.key > n3){
+			return lca2(node.leftChild, n1, n2, n3);
+		}
+		if(node.key < n1 && node.key < n2 && node.key < n3){
+			return lca2(node.rightChild, n1, n2, n3);
+		}
+		return node;
+	}
+}
+
+	class Node {
+		int key;
+		String name;
+
+		Node leftChild;
+		Node rightChild;
+
+		Node(int key, String name) {
+			this.key = key;
+			this.name = name;
+	}
+}
+
+
